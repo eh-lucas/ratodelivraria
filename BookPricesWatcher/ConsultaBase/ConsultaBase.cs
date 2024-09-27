319c2bf58359972235bed13945e3121e585842c4
Lucas Hergessel <lucas.hergessel@gmail.com>
2024-09-26 23:04:53 -0300

    continua selecao de itens

--- BookPricesWatcher/ConsultaBase/ConsultaBase.cs
+++ BookPricesWatcher/ConsultaBase/ConsultaBase.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BookPricesWatcher.Model;
 using OpenQA.Selenium.Support.UI;
 
 namespace BookPricesWatcher.ConsultaBase;
@@ -13,7 +14,7 @@ class ConsultaBase
 
     public async void Execute()
     {
-
+        List<Book> books = new List<Book>();
         ChromeOptions options = new ChromeOptions();
         options.AddArgument("--start-maximized");
         IWebDriver driver = new ChromeDriver();
@@ -24,17 +25,39 @@ class ConsultaBase
         driver.Navigate().GoToUrl(url);
 
         IWebElement searchBox = driver.FindElement(By.XPath("//*[@id=\"input-search\"]"));
-        if (searchBox.Displayed) 
+        if (searchBox.Displayed)
             searchBox.Click();
 
         searchBox.SendKeys(bookTitle);
 
         var searchButtonElem = driver.FindElement(By.XPath("//*[@id=\"doSearch\"]"));
-        if (searchButtonElem.Displayed) 
+        if (searchButtonElem.Displayed)
             searchButtonElem.Click();
 
-        IWebElement grid = driver.FindElement(By.XPath(""))
+        IWebElement grid = driver.FindElement(By.XPath("//*[@id=\"column-right\"]/div[5]"));
+        IReadOnlyList<IWebElement> itemProductElement = grid.FindElements(By.ClassName("item-product"));
+        foreach (var element in itemProductElement)
+        {
+            var elementName = element.FindElement(By.ClassName("name"));
+            if (elementName.Text.Contains(bookTitle))
+            {
+
+                string author = element.FindElement(By.ClassName("author")).Text;
+                string priceNew = element.FindElement(By.ClassName("price-new")).Text;
+                try
+                {
+                    string oldPrice = element.FindElement(By.ClassName("price-old")).Text;
+                }
+                catch
+                {
+                    string oldPrice = priceNew;
+                }
+
+                books.Add(new Book(bookTitle, author, Convert.ToDouble(priceNew),dif);
+            }
+        }
     }
+
     private async Task LoadPageAsync(IWebDriver driver, string url)
     {
         // Navegar para a URL sem Task.Run
--- BookPricesWatcher/Model/Book.cs
+++ BookPricesWatcher/Model/Book.cs
@@ -5,10 +5,17 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace BookPricesWatcher.Model;
-    class Book
+class Book
+{
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public double Price { get; set; }
+    public int Discount { get; set; }
+    public Book(string title, string author, double price, int discount)
     {
-        public string Title { get; set; }
-        public string Author { get; set; }
-        public int Price { get; set; }
-
+        Title = title;
+        Author = author;
+        Price = price;
+        Discount = discount;
     }
+}
