d2087e2e8c3b9fdccc5ec62f5d5ec4ce166f0e6f
vitor.rosa <vitor.rosa@mps.com.br>
2025-10-27 15:26:02 -0300

    Brincando com o componente de input personalizado, ainda não funciona

--- Client/src/app/app.ts
+++ Client/src/app/app.ts
@@ -2,9 +2,10 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { InputComponent } from './components/input/input-component'
 import { LinkButtonComponent } from './components/link-button-component/link-button-component'
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, LinkButtonComponent, InputComponent],
+  imports: [RouterOutlet, LinkButtonComponent, InputComponent ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
--- Client/src/app/components/input/input-component.ts
+++ Client/src/app/components/input/input-component.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'input[custom-input]',
-  imports: [],
-  template: '<ng-content/>',
-  styleUrl: './input-component.scss'
+  standalone: true,
+  imports: [FormsModule],
+  template: `
+    <div class="custom-input-wrapper">
+
+      @if (iconSrc) { <img class="input-icon-img" [src]="iconSrc" alt="icon"> }
+
+      @if (hint) { <span class="input-hint">{{ hint }}</span> }
+
+      <input class="input-placeholder" [attr.placeholder]="placeholder" [(ngModel)]="value">
+
+    </div>
+  `,
+  styleUrls: ['./input-component.scss']
 })
 export class InputComponent {
-
+  @Input() iconSrc?: string;
+  @Input() hint?: string;
+  @Input() placeholder?: string;
+  @Input() value: string = '';
 }
