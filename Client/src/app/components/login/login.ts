04a075363ca0f1f082d9e9aac97716a23fae1d6c
vitor.rosa <vitor.rosa@mps.com.br>
2025-10-28 09:40:55 -0300

    Código incompleto mas preciso salvar as alterações (ainda implementando auth login)

--- Client/src/app/components/login/login.ts
+++ Client/src/app/components/login/login.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Auth } from '../../services/auth';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,5 +9,20 @@ import { Component } from '@angular/core';
   styleUrl: './login.scss'
 })
 export class Login {
+  username: string = '';
+  password: string = '';
 
+  constructor(private authService: Auth, private router: Router) { }
+
+  onLogin(): void {
+    this.authService.login(this.username, this.password).subscribe({
+      next: (response) => {
+        this.authService.setToken(response.token);
+        this.router.navigate(['/home']); // não temos essa rota, AINDA
+      },
+      error: (err) => {
+        console.error('Login failed.', err);
+      }
+    });
+  }
 }
